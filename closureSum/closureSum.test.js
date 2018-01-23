b7dfe575ea22d88b7217362069c36f4e58528587
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-23 22:50:02 +0530

    Added second test for closureSum.test.js

--- closureSum/closureSum.test.js
+++ closureSum/closureSum.test.js
@@ -1,10 +1,16 @@
 const closureSum = require('./closureSum');
 
 describe('Testing closure function', () => {
-  it('testing with a valid number', () => {
+  it('testing with valid numbers', () => {
     const testNum = 2;
     const testClosureNum = 3;
     const result = 5;
+    expect(closureSum(testNum)(testClosureNum)).toBe(result);
+  });
+  it('testing with a valid number and undefined', () => {
+    const testNum = 2;
+    const testClosureNum = undefined;
+    const result = NaN;
     expect(closureSum(testNum)(testClosureNum)).toBe(result);
   });
 });
