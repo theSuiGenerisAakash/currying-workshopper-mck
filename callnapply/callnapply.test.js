bca300d251e4adecc87fcb3caf3f63313fbe51ba
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-28 19:01:14 +0530

    Added test when call is passed undefined

--- callnapply/callnapply.test.js
+++ callnapply/callnapply.test.js
@@ -14,4 +14,13 @@ describe('testing call function', () => {
     callnapply.caller(this, outer, name, age, tee);
     expect(outer.call).toHaveBeenCalledWith(this, name, age, tee);
   });
+  it('testing Function.prototype.call as mock function', () => {
+    const outer = function () {};
+    outer.call = jest.fn();
+    // const name = 'Aakash';
+    // const age = 22;
+    // const tee = 'M';
+    callnapply.caller(this, outer);
+    expect(outer.call).toHaveBeenCalledWith(this, undefined, undefined, undefined);
+  });
 });
