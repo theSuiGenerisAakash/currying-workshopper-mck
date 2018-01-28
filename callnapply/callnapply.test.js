9cb893799a148fb3ef6a415a17cd78f9ca68b40b
Aakash Verma <aku06_ran@yahoo.co.in>
2018-01-28 18:59:09 +0530

    NOW TESTING CALL AND APPLY - TEST 1

--- callnapply/callnapply.test.js
+++ callnapply/callnapply.test.js
@@ -6,16 +6,12 @@ describe('testing call function', () => {
     expect(typeof callnapply.caller(this, func)).toBe('undefined');
   });
   it('testing Function.prototype.call as mock function', () => {
-    const outer = jest.fn((() => {}).call); // blocker
+    const outer = function () {};
+    outer.call = jest.fn();
     const name = 'Aakash';
     const age = 22;
     const tee = 'M';
     callnapply.caller(this, outer, name, age, tee);
-    expect(outer).toBeCalled();
+    expect(outer.call).toHaveBeenCalledWith(this, name, age, tee);
   });
-  // it('testing with undefined values', () => {
-  //   const outer = jest.fn(Function.prototype.call()); // blocker
-  //   callnapply.caller(this, outer);
-  //   expect(outer).toHaveBeenCalledWith(undefined, undefined, undefined);
-  // });
 });
--- currying/currying.js
+++
@@ -1,7 +0,0 @@
-const currying = (fn) => {
-  const paramLength = fn.length;
-  return (...args) => (args.length > paramLength ? fn(...args)
-    : (...args2) => fn(args.concat(args2)));
-};
-
-module.exports = currying;
